57efdcfc91f1d73adbbb5e5a41ec36b789716672
Sahil <sahilsharma2000dtc@gmail.com>
2024-10-06 22:24:26 +0530

    added pkg dir and config file to read the configuration from the env to start the application

---
+++ pkg/config/config.go
@@ -0,0 +1,58 @@
+//config file for serilization with yaml file in ./config/local.yml
+
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HttpServer struct {
+	Address string
+}
+
+/*
+	this is used in production environment as:
+	ENV string `yaml:"env" env-default:"production"`
+	env-default:"production"
+*/
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HttpServer  `yaml:"http_server"`
+}
+
+// this mustload fnction have to run before main to get the config if not then stop the application right there.
+func MustLoadConfig() *Config {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the config file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("CONFIG_PATH is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("cannot read config file: %s", err.Error())
+	}
+
+	return &cfg
+}
