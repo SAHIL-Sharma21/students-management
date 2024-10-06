e2ea0e9d40013ebba6a3a87dcdbd133683ffdb46
Sahil <sahilsharma2000dtc@gmail.com>
2024-10-06 21:02:14 +0530

    Initial commit and project setup:

---
+++ cmd/students-management/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	//code here...
+	fmt.Println("students management backend system")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SAHIL-Sharma21/students-management
+
+go 1.23.1
